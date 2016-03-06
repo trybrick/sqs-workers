d723a5b2f15df2d3dc1489a656c6e83e06c12e88
noogen <tom@noogen.net>
2016-03-06 15:54:51 -0600

    complete delete

--- workers/ftp-delete/index.js
+++ workers/ftp-delete/index.js
@@ -1,23 +1,49 @@
+var async = require('async');
 var request = require('request');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 
 // this is not a long running job and it should be on AWS Lambda
 // we have it here as an example of a sqs-workers job
 module.exports = {
   handler: function(event, context) {
-    var auth = "Basic " + new Buffer(config.FTP_API_KEY + ":x").toString("base64");
-    var basePath = 'https://brickinc.brickftp.com/files';
-    var filePath = event.FilePath.replace(/^\/+|\/+$/gi, '');
-    var opts = {
-      method: 'DELETE',
-      uri: `${basePath}/${filePath}`,
-      headers: {
-        "Authorization": auth
-      }
+    console.log('processing', JSON.stringify(event, null, 2));
+    var eventRecord = event.Records && event.Records[0];
+    var record = eventRecord.s3 || eventRecord.custom;
+    var srcBucket = record.bucket.name;
+    var srcKey = decodeURIComponent(
+      record.object.key.replace(/\+/g, ' ')
+    );
+    var bucketFrom = {
+      Bucket: srcBucket,
+      Key: srcKey
     };
 
-    request(opts, function(error, response, body) {
-      // console.log(response);
-      context.done(error);
+    async.waterfall([
+      function download(next) {
+        s3.headObject(bucketFrom, next);
+      },
+      function deleteFile(response, next) {
+        var filePath = response.Metadata.ftp.replace(/^\/+|\/+$/gi, '');
+        var auth = "Basic " + new Buffer(config.FTP_API_KEY + ":x").toString("base64");
+        var opts = {
+          method: 'DELETE',
+          uri: `https://brickinc.brickftp.com/files/${filePath}`,
+          headers: {
+            "Authorization": auth
+          }
+        };
+
+        request(opts, next);
+      }
+    ], function(err) {
+      if (err) {
+        console.error('Failed to delete ftp file: ', err);
+      } else {
+        console.log('Successfully delete ftp file.');
+      }
+
+      context.done(err);
     });
   }
 };
