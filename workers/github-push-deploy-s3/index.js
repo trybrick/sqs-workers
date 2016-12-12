79fdfc321cc38532f4c26ecd3a4f104ad27e9391
Tom Noogen <tom@tnguyen-imac.local>
2016-12-12 11:43:16 -0600

    attempt to update github push deploy

--- workers/github-push-deploy-s3/index.js
+++ workers/github-push-deploy-s3/index.js
@@ -8,12 +8,12 @@ var mkdirp = require('mkdirp');
 var _ = require('lodash');
 var fs = require('fs');
 var exec = require('child_process').exec;
-
+var path = require('path');
 var config = {};
 var today = new Date();
-var myDir = '/tmp/deploy/';
-
-var log = function() {
+var myDir = 'tmp';
+myDir = path.combine(__dirname, myDir);
+var log = function () {
     // do some custom log recording
     // log.call(this, 'My Console!!!');
     var args = Array.prototype.slice.call(arguments);
@@ -22,9 +22,8 @@ var log = function() {
 
 function doDownload() {
     log('start download');
-
     // execute aws-cli s3 sync
-    return new Promise(function(Y, N) {
+    return new Promise(function (Y, N) {
         mkdirp.sync(myDir);
         var data = config.data;
         var ref = config.ref;
@@ -32,10 +31,10 @@ function doDownload() {
         var cmd = spawn('curl', ['-Lk', '-o', `${myDir}/result.tar.gz`, downloadUrl], {
             cwd: myDir
         });
-        cmd.stdout.on('data', function(data) {
+        cmd.stdout.on('data', function (data) {
             log('' + data);
         });
-        cmd.on('close', function(code) {
+        cmd.on('close', function (code) {
             code == 0 ? Y(code) : N(code);
         });
         cmd.on('error', N);
@@ -44,16 +43,15 @@ function doDownload() {
 
 function doExtract(downloadPath, ref, callback) {
     log('start extract');
-
     // execute aws-cli s3 sync
-    return new Promise(function(Y, N) {
+    return new Promise(function (Y, N) {
         var cmd = spawn('tar', ['-xvf', 'result.tar.gz', `--strip=1`], {
             cwd: myDir
         });
-        cmd.stdout.on('data', function(data) {
+        cmd.stdout.on('data', function (data) {
             log('' + data);
         });
-        cmd.on('close', function(code) {
+        cmd.on('close', function (code) {
             code == 0 ? Y(code) : N(code);
         });
         cmd.on('error', N);
@@ -65,15 +63,13 @@ function cleanUp() {
         context.done('invalid work dir: ' + myDir);
         return;
     }
-
     log('start cleanUp', myDir);
-
     // exec filehose
-    return new Promise(function(Y, N) {
+    return new Promise(function (Y, N) {
         var cmd = spawn('bash', ['-c', 'rm -rf *'], {
             cwd: myDir
         });
-        cmd.stdout.on('data', function(data) {
+        cmd.stdout.on('data', function (data) {
             log('' + data);
         });
         cmd.on('close', Y);
@@ -83,34 +79,30 @@ function cleanUp() {
 
 function makeExec() {
     log('make exec');
-
     // execute aws-cli s3 sync
-    return new Promise(function(Y, N) {
+    return new Promise(function (Y, N) {
         if (!fs.existsSync(myDir + '/deploy.sh')) {
             console.log('skipping: no deploy.sh');
             Y();
             return;
         }
-
         var cmd = spawn('chmod', ['+x', 'deploy.sh'], {
             cwd: myDir
         });
-        cmd.stdout.on('data', function(data) {
+        cmd.stdout.on('data', function (data) {
             log('' + data);
         });
-        cmd.on('close', function(code) {
+        cmd.on('close', function (code) {
             code == 0 ? Y(code) : N(code);
         });
         cmd.on('error', N);
     });
 }
 
-
 function syncToS3() {
     log('start syncToS3');
-
     // execute aws-cli s3 sync
-    return new Promise(function(Y, N) {
+    return new Promise(function (Y, N) {
         log('sourceDir', myDir);
         /* troubleshoot permission issue 
         var cmd = './deploy.sh ' + config.ref;
@@ -118,14 +110,13 @@ function syncToS3() {
             cwd: myDir
         }, Y);
         */
-
         var cmd = spawn('./deploy.sh', [config.ref], {
             cwd: myDir
         });
-        cmd.stdout.on('data', function(data) {
+        cmd.stdout.on('data', function (data) {
             log('data', '' + data);
         });
-        cmd.on('close', function(code) {
+        cmd.on('close', function (code) {
             console.log('close');
             code == 0 ? Y(code) : N(code);
         });
@@ -137,55 +128,37 @@ function logResult(err) {
     if (err) {
         log('error', err);
     }
-    
     log('done');
     // TODO: log somewhere
     // log('uploading process log...');
-
     setTimeout(config.context.done, 1000);
 }
-
 module.exports = {
-    handler: function(event, context) {
+    handler: function (event, context) {
         console.log('processing', JSON.stringify(event, null, 2));
         config.context = context;
-
         var ghevent = (event.MessageAttributes['X-Github-Event']['Value'] + '').toLowerCase() || 'unknown';
         if (ghevent !== 'push') {
             logResult('This service only handle push event.');
             return;
         }
-
         config.data = JSON.parse(event.Message);
-
         var ref = config.data.ref;
-        if (config.data.error)
-        {
+        if (config.data.error) {
             log('message error', config.data.error);
-            
             config.data = config.message;
             ref = config.data.ref;
         }
-        
         if (!ref) {
             logResult('ref is undefined!');
             return;
         }
-
-
         ref = ref.replace('refs/heads/', '');
         config.ref = ref;
-
         if (0 > ['tst', 'uat', 'master', 'production'].indexOf(ref)) {
             logResult('This service only handle tst/uat/master/production branch.');
             return;
         }
-
-        cleanUp()
-            .then(doDownload)
-            .then(doExtract)
-            .then(makeExec)
-            .then(syncToS3)
-            .then(logResult, logResult);
+        cleanUp().then(doDownload).then(doExtract).then(makeExec).then(syncToS3).then(logResult, logResult);
     }
 };
