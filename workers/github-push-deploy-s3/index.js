5e7b03b7ca56c816870e6dc2d2ba955cc5ed5561
Tom Noogen <tom@tnguyen-imac.local>
2016-12-12 13:35:12 -0600

    update

--- workers/github-push-deploy-s3/index.js
+++ workers/github-push-deploy-s3/index.js
@@ -71,7 +71,7 @@ function cleanUp() {
     // exec filehose
     return new Promise(function (Y, N) {
         log('start cleanUp', myDir);
-        var cmd = spawn(bash, ['../deleteTemp.sh'], {
+        var cmd = spawn('rm', ['-rf', myDir, '/*'], {
             cwd: myDir
         });
         cmd.stdout.on('data', function (data) {
