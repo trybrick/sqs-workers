c93951b75463ff7ae4b67cb6bd9b015c94eb2c64
Tom Noogen <tom@tnguyen-imac.local>
2016-12-15 14:07:53 -0600

    update

--- workers/pos-archive/index.js
+++ workers/pos-archive/index.js
@@ -84,7 +84,7 @@ function splitFiles(filePath) {
     return new Promise(function (Y, N) {
         var newDir = path.resolve(myDir + "\\..\\..\\..\\..");
         log('start splitFiles', newDir);
-        var cmd = spawn(newDir + '\\node_modules\\.bin\\filehose.comd', [configFile, filePath], {
+        var cmd = spawn(newDir + '\\node_modules\\.bin\\filehose.cmd', [configFile, filePath], {
             cwd: myDir
         });
         cmd.stdout.on('data', function (data) {
